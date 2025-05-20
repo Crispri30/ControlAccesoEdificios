bdbf5e0b62c8dcf3d83e52b3bbfe9e8d81012b0b
Cristian Primera <cristian.primera789@pascualbravo.edu.co>
2025-05-20 10:40:06 -0500

    Clase conexionBD con patron singleton

--- ControlEdificios/ControlEdificios/obj/Debug/net8.0-windows/ControlEdificios.AssemblyInfo.cs
+++ ControlEdificios/ControlEdificios/obj/Debug/net8.0-windows/ControlEdificios.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ControlEdificios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab065b72084835d2048f1a07e1ae9c45888896b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ControlEdificios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ControlEdificios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
