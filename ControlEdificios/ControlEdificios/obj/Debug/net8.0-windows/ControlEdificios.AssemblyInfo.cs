893e10b3c2cf83d6e4ecab5b2f31e71f1afe925b
Manuela Castaño G <manuela.castano585@pascualbravo.edu.co>
2025-05-20 13:53:39 -0500

    Creación de clases en Repositorios: GenerarAlerta, Visitante

--- ControlEdificios/ControlEdificios/obj/Debug/net8.0-windows/ControlEdificios.AssemblyInfo.cs
+++ ControlEdificios/ControlEdificios/obj/Debug/net8.0-windows/ControlEdificios.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ControlEdificios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55973c4fb35eb559de614abd3ef0a52481195174")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28cccbd74103b3205466182e79525189a7eec479")]
 [assembly: System.Reflection.AssemblyProductAttribute("ControlEdificios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ControlEdificios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
