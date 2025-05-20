21091a057386f845fb3efd18777f74c23b6b89ce
Juan  Ardila <camiloardila.1803@gmail.com>
2025-05-20 14:31:21 -0500

    Se crean los forms para la interacción con el sistema

---
+++ ControlEdificios/ControlEdificios/Forms/FormAdministrador.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ControlAccesoPrueba.Repositorios;
+using ControlAccesoPrueba.Modelos;
+
+namespace ControlAccesoPrueba.Forms
+{
+    public partial class FormAdministrador : Form
+    {
+        public FormAdministrador()
+        {
+            InitializeComponent();
+        }
+
+        private void FormAdministrador_Load(object sender, EventArgs e)
+        {
+            cmbRol.DataSource = Enum.GetValues(typeof(RolEmpleado));
+        }
+
+        private void bttAgregar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int empleadoID = int.Parse(txtID.Text);
+                string nombre = txtNombre.Text;
+                string zonaAcceso = txtZona.Text;
+
+                if (!Enum.TryParse(cmbRol.SelectedItem.ToString(), out RolEmpleado rolseleccionado))
+                {
+                    MessageBox.Show("Error: Tipo de rol no válido.");
+                }
+
+                var repo = new EmpleadoRepository();
+                repo.AgregarEmpleado(empleadoID, nombre, rolseleccionado, zonaAcceso);
+                MessageBox.Show("Empleado agregado correctamente");
+
+                txtID.Clear();
+                txtNombre.Clear();
+                txtZona.Clear();
+                cmbRol.SelectedIndex = 0;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void bttEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int empleadoID = int.Parse(txtID.Text);
+                var repo = new EmpleadoRepository();
+                repo.EliminarEmpleado(empleadoID);
+
+                txtID.Clear();
+                txtNombre.Clear();
+                txtZona.Clear();
+                cmbRol.SelectedIndex = 0;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void bttActualizar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int empleadoID = int.Parse(txtID.Text);
+                string nombre = txtNombre.Text;
+                string zonaAcceso = txtZona.Text;
+
+                if (!Enum.TryParse(cmbRol.SelectedItem.ToString(), out RolEmpleado rolseleccionado))
+                {
+                    MessageBox.Show("Error: Tipo de rol no válido.");
+                }
+
+                var repo = new EmpleadoRepository();
+                repo.ActualizarEmpleado(empleadoID, nombre, rolseleccionado, zonaAcceso);
+
+                txtID.Clear();
+                txtNombre.Clear();
+                txtZona.Clear();
+                cmbRol.SelectedIndex = 0;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void bttAgregarVis_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int visitanteID = int.Parse(txtIDVis.Text);
+                string nombreVisitante = txtNombreVis.Text;
+                string telefono = txtTelefono.Text;
+                string motivoVisita = txtMotivoVisita.Text;
+
+                var repo = new VisitanteRepository();
+                repo.AgregarVisitante(visitanteID, nombreVisitante, telefono, motivoVisita);
+
+                txtIDVis.Clear();
+                txtNombreVis.Clear();
+                txtTelefono.Clear();
+                txtMotivoVisita.Clear();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void bttActualizarVis_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int visitanteID = int.Parse(txtIDVis.Text);
+                string nombreVisitante = txtNombreVis.Text;
+                string telefono = txtTelefono.Text;
+                string motivoVisita = txtMotivoVisita.Text;
+
+                var repo = new VisitanteRepository();
+                repo.ActualizarVisitantes(visitanteID, nombreVisitante, telefono, motivoVisita);
+
+                txtIDVis.Clear();
+                txtNombreVis.Clear();
+                txtTelefono.Clear();
+                txtMotivoVisita.Clear();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void bttEliminarVis_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int visitanteID = int.Parse(txtIDVis.Text);
+              
+                var repo = new VisitanteRepository();
+                repo.EliminarVisitantes(visitanteID);
+
+                txtIDVis.Clear();
+                txtNombreVis.Clear();
+                txtTelefono.Clear();
+                txtMotivoVisita.Clear();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
--- ControlEdificios/ControlEdificios/obj/Debug/net8.0-windows/ControlEdificios.AssemblyInfo.cs
+++ ControlEdificios/ControlEdificios/obj/Debug/net8.0-windows/ControlEdificios.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ControlEdificios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f11152c414db1ad7abfcd7a22d3db05acb2fb234")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fce1db7e6e33d3bafaf1aacc6c8b64c2bc8880ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("ControlEdificios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ControlEdificios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
