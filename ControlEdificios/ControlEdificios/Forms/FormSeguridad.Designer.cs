bf26d205afa209d80627d779c877680d1022cde8
Cristian Primera <cristian.primera789@pascualbravo.edu.co>
2025-06-03 13:57:25 -0500

    Se mejora el form seguridad

--- ControlEdificios/ControlEdificios/Forms/FormSeguridad.Designer.cs
+++ ControlEdificios/ControlEdificios/Forms/FormSeguridad.Designer.cs
@@ -31,16 +31,18 @@
             components = new System.ComponentModel.Container();
             dgvAccesosActivos = new DataGridView();
             timerMonitoreo = new System.Windows.Forms.Timer(components);
+            label11 = new Label();
             ((System.ComponentModel.ISupportInitialize)dgvAccesosActivos).BeginInit();
             SuspendLayout();
             // 
             // dgvAccesosActivos
             // 
             dgvAccesosActivos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvAccesosActivos.Location = new Point(81, 74);
+            dgvAccesosActivos.Location = new Point(94, 66);
+            dgvAccesosActivos.Margin = new Padding(2, 2, 2, 2);
             dgvAccesosActivos.Name = "dgvAccesosActivos";
             dgvAccesosActivos.RowHeadersWidth = 62;
-            dgvAccesosActivos.Size = new Size(681, 382);
+            dgvAccesosActivos.Size = new Size(629, 357);
             dgvAccesosActivos.TabIndex = 0;
             // 
             // timerMonitoreo
@@ -49,24 +51,36 @@
             timerMonitoreo.Interval = 5000;
             timerMonitoreo.Tick += timerMonitoreo_Tick;
             // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label11.Location = new Point(190, 9);
+            label11.Name = "label11";
+            label11.Size = new Size(389, 30);
+            label11.TabIndex = 26;
+            label11.Text = "PANEL MONITOREO EN TIEMPO REAL";
+            // 
             // FormSeguridad
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.PeachPuff;
-            ClientSize = new Size(1143, 750);
+            ClientSize = new Size(800, 449);
+            Controls.Add(label11);
             Controls.Add(dgvAccesosActivos);
-            Margin = new Padding(4, 5, 4, 5);
             Name = "FormSeguridad";
             Text = "FormSeguridad";
             Load += FormSeguridad_Load;
             ((System.ComponentModel.ISupportInitialize)dgvAccesosActivos).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private DataGridView dgvAccesosActivos;
         private System.Windows.Forms.Timer timerMonitoreo;
+        private Label label11;
     }
 }
